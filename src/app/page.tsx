bebd79d0e883a7a36ff4b699d8867caeac6aa865
Soonyoung Park <soon.syp@gmail.com>
2025-03-23 22:51:37 +0100

    correct type errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { faBookmark, faCalendar, faCircleUser, faUser } from '@fortawesome/free-
 import { faMagnifyingGlass, faLocationDot, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import Exhibition from '@/components/Exhibition'
 import Calendar from '@/components/Calendar'
+import NumInput from '@/components/NumInput'
 
 export default function Home() {
   // const router = useRouter()
@@ -17,12 +18,31 @@ export default function Home() {
   const [searchClicked, setsearchClicked] = useState<boolean>(false)
   const [where, setWhereValue] = useState<string>('Search')
   const [when, setWhenValue] = useState<string>('Add dates')
-  const [who, setWhoValue] = useState<string>('Add number')
+  const [whoNum, setWhoNum] = useState<number>(0)
   const whereRef = useRef<HTMLDivElement>(null)
   const whenRef = useRef<HTMLDivElement>(null)
   const whoRef = useRef<HTMLDivElement>(null)
+  const today = new Date()
+  const [month, setMonth] = useState<number>(2)
+  const [year, setYear] = useState<number>(today.getFullYear())
 
-  console.log(searchClicked)
+  const increment = () => {
+    if (month === 11) {
+      setMonth(0)
+      setYear((prev) => prev + 1)
+    } else {
+      setMonth((prev) => prev + 1)
+    }
+
+  }
+  const decrement = () => {
+    if (month === 0) {
+      setMonth(11)
+      setYear((prev) => prev - 1)
+    } else {
+      setMonth((prev) => prev - 1)
+    }
+  }
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -71,7 +91,15 @@ export default function Home() {
                 onMouseLeave={(e) => e.currentTarget.style.backgroundColor = showWhen ? 'white' : 'transparent'} >
                 <h3>When</h3>
                 <p>{when}</p>
-                {showWhen && 'Add dates' && <SelectWhen width={'var(--serachbar-width)'} setValue={setWhenValue} />}
+                {showWhen &&
+                  <div className='web px-4 py-3' style={{
+                    backgroundColor: 'white', width: '380px', borderRadius: '2rem', zIndex: 100,
+                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', left: '50%', transform: 'translateX(-50%)',
+                  }}>
+                    {!(month === today.getMonth() && year === today.getFullYear()) && <FontAwesomeIcon onClick={decrement} icon={faChevronLeft} style={{ position: 'absolute', top: '2rem', left: '2rem' }} />}
+                    <FontAwesomeIcon onClick={increment} icon={faChevronRight} style={{ position: 'absolute', top: '2rem', right: '2rem' }} />
+                    <Calendar year={year} month={month} setValue={setWhenValue}/>
+                  </div>}
               </div>
               <div className={`search-item ${showWho ? 'clicked' : ''}`} ref={whoRef} onClick={() => { setWho(true) }} style={{
                 backgroundColor: showWho ? "white" : "transparent"
@@ -79,13 +107,14 @@ export default function Home() {
                 onMouseOver={(e) => e.currentTarget.style.backgroundColor = showWho ? 'trnaparent' : '#d5d5d5b4'}
                 onMouseLeave={(e) => e.currentTarget.style.backgroundColor = showWho ? 'white' : 'transparent'} >
                 <h3>Who</h3>
-                <p>{who === '0' ? 'Add number' : who}</p>
+                {whoNum === 0 && <p>Add number</p>}
+                {whoNum > 0 && <p>{whoNum}</p>}
                 {showWho &&
-                  <div className='web' style={{
-                    backgroundColor: 'white', width: 'var(--serachbar-width)', height: '60px', borderRadius: '2rem', zIndex: 100,
-                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', left: '50%', transform: 'translateX(-50%)',
+                  <div className='web web-who-box' style={{
+                    backgroundColor: 'white', width: 'auto', height: '60px', borderRadius: '2rem', zIndex: 100,
+                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', transform: 'translateX(-75%)',
                   }}>
-                    <SelectWho setValue={setWhoValue} initial={who === 'Add number' ? 0 : who} />
+                    <NumInput setValue={setWhoNum} initial={whoNum} />
                   </div>}
               </div>
             </div>
@@ -130,104 +159,24 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {showWhere && <SelectWhere width={'var(--serachbar-width)'} height={'50%'} />}
-      {searchClicked && 
-      
-      <div className='mobile' style={{
-        position: 'fixed', top: 0, left: 0, zIndex: 100,
-        width: '100vw', height: '100vh', backgroundColor: 'white'
-      }}>
-        <SelectWho setValue={setWhoValue} initial={who === 'Add number' ? 0 : who} />
-      </div>
-      
-      
-      }
-    </>
-  )
-}
+      {showWhere &&
+        <div className='web p-3 web-where-box rounded-3xl' style={{
+          overflowY: 'scroll',
+          backgroundColor: 'white', width: '400px', maxHeight: '50%', height: 'auto', zIndex: 100,
+          boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', transform: 'translateX(0%)',
+        }}>
+          <div className='flex items-center hover:bg-gray-100 p-3 rounded-3xl'>
+            <FontAwesomeIcon icon={faLocationDot} className='pr-2' /> <p>postcode</p>
+          </div>
+        </div>}
+      {searchClicked &&
 
-function SelectWhere(props: any) {
-  return (
-    <>
-      <div className='web p-4' style={{
-        overflowY: 'scroll',
-        backgroundColor: 'white', width: props.width, maxHeight: props.height, height: 'auto', borderRadius: '2rem', zIndex: 100,
-        boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', left: '50%', transform: 'translateX(-50%)',
-      }}>
-        <div className='flex items-center hover:bg-gray-100 p-4 rounded-3xl'>
-          <FontAwesomeIcon icon={faLocationDot} className='pr-2' /> <p>passcode</p>
+        <div className='mobile' style={{
+          position: 'fixed', top: 0, left: 0, zIndex: 100,
+          width: '100vw', height: '100vh', backgroundColor: 'white'
+        }}>
         </div>
-      </div>
-
-    </>
-  )
-}
-
-function SelectWhen(props: any) {
-  const today = new Date()
-  const [month, setMonth] = useState<number>(2)
-  const [year, setYear] = useState<number>(today.getFullYear())
-
-  const increment = () => {
-    if (month === 11) {
-      console.log(month)
-      setMonth(0)
-      setYear((prev) => prev + 1)
-    } else {
-      setMonth((prev) => prev + 1)
-    }
-
-  }
-  const decrement = () => {
-    if (month === 0) {
-      console.log(month)
-      setMonth(11)
-      setYear((prev) => prev - 1)
-    } else {
-      setMonth((prev) => prev - 1)
-    }
-
-  }
-
-  return (
-    <>
-      <div className='web grid grid-cols-[1fr_1fr] gap-4 p-4' style={{
-        backgroundColor: 'white', width: props.width, borderRadius: '2rem', zIndex: 100,
-        boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', left: '50%', transform: 'translateX(-50%)',
-      }}>
-        {!(month === today.getMonth() && year === today.getFullYear()) && <FontAwesomeIcon onClick={decrement} icon={faChevronLeft} style={{ position: 'absolute', top: '2rem', left: '2rem' }} />}
-        <FontAwesomeIcon onClick={increment} icon={faChevronRight} style={{ position: 'absolute', top: '2rem', right: '2rem' }} />
-        <Calendar year={year} month={month} setValue={props.setValue} />
-        <Calendar year={month === 11 ? year + 1 : year} month={month === 11 ? 0 : month + 1} setValue={props.setValue} />
-      </div>
-    </>
-  )
-}
-
-function SelectWho(props: any) {
-  const [value, setValue] = useState<number>(parseInt(props.initial))
-  const increment = () => {
-    setValue((prev) => prev + 1)
-    props.setValue((value + 1).toString())
-  }
-  const decrement = () => {
-    setValue((prev) => (prev > 0 ? prev - 1 : 0))
-    props.setValue((value > 0 ? value - 1 : 0).toString())
-  }
-  return (
-    <>
-
-      <div className='p-3 flex justify-center'>
-        <button onClick={decrement} style={{ marginRight: '0.6rem', opacity: value === 0 ? 0 : 1 }}
-          className="text-xl border border-gray-300 text-gray-600 px-3 py-[3px] hover:border-gray-500 rounded-full cursor-pointer">
-          −
-        </button>
-        <input type="number" value={value} onChange={(e) => setValue(Number(e.target.value))} className="w-16 text-center" />
-        <button onClick={increment}
-          className="text-xl border border-gray-300 text-gray-600 px-3 py-[3px] hover:border-gray-500 rounded-full cursor-pointer" >
-          +
-        </button>
-      </div>
+      }
     </>
   )
 }
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from 'react'
 
-export default function Calendar(props: any) {
+interface props {
+  year: number
+  month: number
+  setValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Calendar: React.FC<props> = ({ year, month, setValue }) => {
   const months: string[] = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ]
   const days: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-  const theFirst = new Date(props.year, props.month, 1)
+  const theFirst = new Date(year, month, 1)
   const theFirstDayOfWeek = (theFirst.getDay() === 0 ? 6 : theFirst.getDay() - 1)
-  const lastDate = new Date(props.year, props.month + 1, 0).getDate()
+  const lastDate = new Date(year, month + 1, 0).getDate()
   const [daysInMonth, setDaysInMonth] = useState<string[]>([])
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
   const [clickedIndex, setClickedIndex] = useState<number | null>(null)
 
   useEffect(() => {
     updateDateArray()
-  }, [props.year, props.month])
+  }, [year, month])
 
   const updateDateArray = () => {
     let daycount: number = 1
@@ -35,13 +41,13 @@ export default function Calendar(props: any) {
   const updateValue = (day: string, index: number) => {
     if (day !== '') {
       setClickedIndex(index)
-      props.setValue(day + " " + months[props.month] + " " + props.year)
+      setValue(day + " " + months[month] + " " + year)
     }
   }
 
   return (
     <div className='w-[100%]'>
-      <div className='font-bold text-center p-3 text-base'>{months[props.month]} {props.year}</div>
+      <div className='font-bold text-center p-3 text-base'>{months[month]} {year}</div>
       <div className='grid grid-cols-7'>
         {days.map((day, index) => (
           <p style={{ fontSize: '0.9rem', textAlign: 'center', paddingBottom: '0.5rem' }} key={index}>{day}</p>
@@ -62,3 +68,5 @@ export default function Calendar(props: any) {
     </div>
   )
 }
+
+export default Calendar
