21adca3562ca194756d7794f2087f03c7e848aee
Soonyoung Park <soon.syp@gmail.com>
2025-03-27 00:10:34 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 import { Quicksand } from "next/font/google"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faReact, faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faFontAwesome, faBookmark, faCalendar, faUser } from '@fortawesome/free-regular-svg-icons'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome, faBookmark, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass, faPersonShelter } from '@fortawesome/free-solid-svg-icons'
 import "./globals.css"
 
 const quicksand = Quicksand({
@@ -29,36 +29,36 @@ export default function RootLayout({
       >
         {children}
         <footer className={`web p-4 xl:px-16 lg:px-8 grid grid-cols-[1fr_60px_24px_24px_24px] gap-4 items-center ${quicksand.className}`}>
-        <a href="https://soonyoung-portfolio.vercel.app/" target="_blank"><p>© 2025 Soonyoung</p></a>
+          <a href="https://soonyoung-portfolio.vercel.app/" target="_blank"><p>© 2025 Soonyoung</p></a>
           <a href="https://nextjs.org/" target="_blank"><Image src="/next.svg" alt="art logo" width={60} height={20} priority /></a>
           <a href="https://react.dev/" target="_blank"><FontAwesomeIcon className='nav-icon' style={{ color: 'deepSkyBlue' }} icon={faReact} /></a>
           <a href="https://fontawesome.com/" target="_blank"><FontAwesomeIcon className='nav-icon' icon={faFontAwesome} /></a>
           <a href="https://github.com/soon-y/art" target="_blank"><FontAwesomeIcon className='nav-icon' icon={faGithub} /></a>
         </footer>
         <div className='mobile nav-bar pt-3 pb-8'>
-                <div className='flex flex-row items-center justify-center'>
-                  <div className='flex-1 flex flex-col items-center justify-center '>
-                    <FontAwesomeIcon className='nav-icon' icon={faMagnifyingGlass} />
-                    <p>Explore</p>
-                  </div>
-                  <div className='flex-1 flex flex-col items-center justify-center '>
-                    <FontAwesomeIcon className='nav-icon' icon={faBookmark} />
-                    <p>Saved</p>
-                  </div>
-                  <div className='flex-1 flex flex-col items-center justify-center '>
-                    <FontAwesomeIcon className='nav-icon' icon={faCalendar} />
-                    <p>e</p>
-                  </div>
-                  <div className='flex-1 flex flex-col items-center justify-center '>
-                    <Image src='/ar.svg' width={30} height={24} alt='ar' />
-                    <p>Docent</p>
-                  </div>
-                  <div className='flex-1 flex flex-col items-center justify-center '>
-                    <FontAwesomeIcon className='nav-icon' icon={faUser} />
-                    <p>Profile</p>
-                  </div>
-                </div>
-              </div>
+          <div className='flex flex-row items-center justify-center'>
+            <div className='flex-1 flex flex-col items-center justify-center '>
+              <FontAwesomeIcon className='nav-icon' icon={faMagnifyingGlass} />
+              <p>Explore</p>
+            </div>
+            <div className='flex-1 flex flex-col items-center justify-center '>
+              <FontAwesomeIcon className='nav-icon' icon={faBookmark} />
+              <p>Bookmark</p>
+            </div>
+            <div className='flex-1 flex flex-col items-center justify-center '>
+              <FontAwesomeIcon className='nav-icon' icon={faPersonShelter} />
+              <p>Visit</p>
+            </div>
+            <div className='flex-1 flex flex-col items-center justify-center '>
+              <Image src='/ar.svg' width={30} height={24} alt='ar' />
+              <p>Docent</p>
+            </div>
+            <div className='flex-1 flex flex-col items-center justify-center '>
+              <FontAwesomeIcon className='nav-icon' icon={faUser} />
+              <p>Profile</p>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
--- src/components/Exhibition.tsx
+++ src/components/Exhibition.tsx
@@ -1,22 +1,24 @@
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookmark  } from '@fortawesome/free-solid-svg-icons'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 export default function Exhibition() {
+  const [bookmarked, setBookmark] = useState<boolean>(false)
+
   return (
     <div className='w-[100%]'>
       <div className='bg-cover bg-center w-[100%] rounded-2xl aspect-[1]' style={{ 
-        backgroundImage: `url('https://picsum.photos/id/122/300/300')`
+        backgroundImage: `url('https://picsum.photos/id/128/300/300')`
         }}>
-          <FontAwesomeIcon className='w-[1rem]' icon={faBookmark} style={{ 
-            position: 'relative', left: 'calc(100% - 2rem)', top: '1rem',  
-            color:'rgba(0, 0, 0, 0.27)', stroke: 'rgba(255,255,255,1)', strokeWidth: '50px',
-            paintOrder: 'fill'}}/>
+          <FontAwesomeIcon icon={faBookmark} onClick={() => setBookmark((prev) => !prev)} style={{ 
+            position: 'relative', left: 'calc(100% - 2rem)', top: '1rem', cursor: 'pointer',
+            color: bookmarked ? 'var(--main)' : 'rgba(0, 0, 0, 0.27)', stroke: 'rgba(255,255,255,1)', strokeWidth: '50px',
+            paintOrder: 'fill', fontSize: '1.2rem', transitionDuration: '500ms'}}/>
         </div>
-      <div style={{ padding: '0 0.2rem' }}>
+      <div style={{ padding: '0 0.2rem', lineHeight: 1.4 }}>
         <h3 style={{ marginTop: '0.5rem'}}>Title</h3>
-        <p>exhibition name</p>
-        <p>Period</p>
-        <p>address</p>
+        <p style={{ color: 'gray' }}>exhibition name</p>
+        <p style={{ color: 'gray' }}>period</p>
         <h3>€ Price</h3>
       </div>
     </div>
