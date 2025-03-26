6f69629366bb1610dfcf262d1ad9e02049dad10c
Soonyoung Park <soon.syp@gmail.com>
2025-03-27 00:10:12 +0100

    add searchbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-"use client"
+'use client'
+
 import Image from 'next/image'
 import { useState, useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
-import { faMagnifyingGlass, faLocationDot, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import Calendar from '@/components/Calendar'
 import NumInput from '@/components/NumInput'
+import SearchAddress from '@/components/SearchAddress'
+import Exhibition from '@/components/Exhibition'
 
 export default function Home() {
   const [showWhere, setWhere] = useState<boolean>(false)
   const [showWhen, setWhen] = useState<boolean>(false)
   const [showWho, setWho] = useState<boolean>(false)
-  const [openWhere, setOpenWhere] = useState<boolean>(false)
+  const [openWhere, setOpenWhere] = useState<boolean>(true)
   const [openWhen, setOpenWhen] = useState<boolean>(false)
   const [openWho, setOpenWho] = useState<boolean>(false)
   const [searchClicked, setsearchClicked] = useState<boolean>(false)
@@ -35,8 +38,8 @@ export default function Home() {
     } else {
       setMonth((prev) => prev + 1)
     }
-
   }
+
   const decrement = () => {
     if (month === 0) {
       setMonth(11)
@@ -66,21 +69,9 @@ export default function Home() {
   }, [])
 
   const openTab = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.currentTarget.id === 'tabWhere') {
-      setOpenWhere(true)
-    } else {
-      setOpenWhere(false)
-    }
-
-    if (e.currentTarget.id === 'tabWhen') {
-      setOpenWhen(true)
-    } else {
-      setOpenWhen(false)
-    }
-
-    if (e.currentTarget.id === 'tabWho') {
-      setOpenWho(true)
-    } else {
+    if (e.currentTarget.id === 'tabWhere') { setOpenWhere(true) } else { setOpenWhere(false) }
+    if (e.currentTarget.id === 'tabWhen') { setOpenWhen(true) } else { setOpenWhen(false) }
+    if (e.currentTarget.id === 'tabWho') { setOpenWho(true) } else {
       setOpenWho(false)
     }
   }
@@ -104,18 +95,7 @@ export default function Home() {
                 onMouseOver={(e) => e.currentTarget.style.backgroundColor = showWhere ? 'trnaparent' : '#d5d5d5b4'}
                 onMouseLeave={(e) => e.currentTarget.style.backgroundColor = showWhere ? 'white' : 'transparent'} >
                 <h3>Where</h3>
-                <input type="text" placeholder='Search' onChange={(e) => setWhereTo(e.target.value)} value={whereTo}/>
-
-                {showWhere &&
-                  <div className='web p-3 web-where-box rounded-4xl' style={{
-                    overflowY: 'scroll',
-                    backgroundColor: 'white', width: '300px', maxHeight: '50%', height: 'auto', zIndex: 100,
-                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px'
-                  }}>
-                    <div className='cursor-pointer flex items-center hover:bg-gray-100 p-3 rounded-3xl' onClick={() => { setWhereTo('Hamburg') }}>
-                      <FontAwesomeIcon icon={faLocationDot} className='pr-2' /> <p>Hamburg</p>
-                    </div>
-                  </div>}
+                <SearchAddress showWhere={showWhere} openWhere={openWhere} whereTo={whereTo} setWhereTo={setWhereTo} />
               </div>
               <div className={`search-item ${showWhen ? 'clicked' : ''}`} ref={whenRef} onClick={() => { setWhen(true) }} style={{
                 backgroundColor: showWhen ? "white" : "transparent"
@@ -123,12 +103,11 @@ export default function Home() {
                 onMouseOver={(e) => e.currentTarget.style.backgroundColor = showWhen ? 'trnaparent' : '#d5d5d5b4'}
                 onMouseLeave={(e) => e.currentTarget.style.backgroundColor = showWhen ? 'white' : 'transparent'} >
                 <h3>When</h3>
-                {whenDay === 0 && <p>Add date</p>}
-                {whenDay !== 0 && <p>{whenDay} {months[whenMonth]} {whenYear}</p>}
+                {whenDay === 0 ? <p style={{ color: 'gray' }}>Add date</p> : <p>{whenDay} {months[whenMonth]} {whenYear}</p>}
                 {showWhen &&
                   <div className='web px-4 py-3' style={{
                     backgroundColor: 'white', width: '380px', borderRadius: '2rem', zIndex: 100,
-                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', left: '50%', transform: 'translateX(-50%)',
+                    boxShadow: '0 1px 10px rgba(0,0,0,0.1)', position: 'fixed', top: '148px', left: '50%', transform: 'translateX(-50%)',
                   }}>
                     {!(month === today.getMonth() && year === today.getFullYear()) &&
                       <FontAwesomeIcon onClick={decrement} icon={faChevronLeft} className='p-6' style={{ position: 'absolute', top: '4px', left: 0 }} />}
@@ -142,56 +121,68 @@ export default function Home() {
                 onMouseOver={(e) => e.currentTarget.style.backgroundColor = showWho ? 'trnaparent' : '#d5d5d5b4'}
                 onMouseLeave={(e) => e.currentTarget.style.backgroundColor = showWho ? 'white' : 'transparent'} >
                 <h3>Who</h3>
-                {whoNum === 0 && <p>Add number</p>}
-                {whoNum > 0 && <p>{whoNum}</p>}
+                {whoNum === 0 ? <p style={{ color: 'gray' }}>Add number</p> : <p>{whoNum}</p>}
                 {showWho &&
                   <div className='web web-who-box' style={{
                     backgroundColor: 'white', width: 'auto', height: '60px', borderRadius: '2rem', zIndex: 100,
-                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)', position: 'fixed', top: '148px', transform: 'translateX(-75%)',
+                    boxShadow: '0 1px 10px rgba(0,0,0,0.1)', position: 'fixed', top: '148px', transform: 'translateX(-75%)',
                   }}>
                     <NumInput setValue={setWhoNum} initial={whoNum} />
                   </div>}
               </div>
             </div>
-            <FontAwesomeIcon icon={faMagnifyingGlass} className='web search-icon' style={{
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='web search-icon duration-500' style={{
               color: 'white', padding: '0.8rem', borderRadius: '50%', transform: 'translate(0%,-50%)'
             }} />
           </div>
           <div className='mobile search-bar cursor-pointer' onClick={() => { setsearchClicked(true) }}>
-            <div className='flex flex-row justify-center items-center'>
-              <FontAwesomeIcon className='w-4' icon={faMagnifyingGlass} />
-              <p className='p-2'>Start your search</p>
+            <div>
+              {whereTo === '' && whoNum === 0 && whenDay === 0 ?
+                <div className='flex flex-row  justify-center items-center'>
+                  <FontAwesomeIcon className='w-4' icon={faMagnifyingGlass} />
+                  <p className='p-2'>Start your search</p>
+                </div> :
+                <div className='justify-center items-center text-center'>
+                  <p>
+                    {whereTo === '' ? 'Where to?' : whereTo}
+                  </p>
+                  <p style={{ fontSize: '0.9rem', color: 'gray' }}>
+                    {whenDay === 0 ? 'Anytime ' : `${whenDay} ${months[whenMonth]}`} &#160;
+                    {whoNum === 0 ? '' : whoNum === 1 ? `1 person` : `${whoNum} people`}
+                  </p>
+                </div>}
             </div>
           </div>
         </header>
         <div className='content-wrapper p-6 xl:px-16 lg:px-8 w-screen'>
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-5 gap-y-8'>
-
+            <Exhibition />
           </div>
         </div>
       </div>
       {searchClicked &&
-        <div className='mobile pt-8 p-4  bg-gray-100 sm:p-16 sm:pt-8' style={{
-          position: 'fixed', top: 0, left: 0, zIndex: 100, width: '100vw', height: '100vh'
+        <div className='mobile pt-8 p-4 sm:p-16 sm:pt-8 bg-gray-100 overflow-y-auto' style={{
+          position: 'fixed', top: 0, left: 0, zIndex: 10, width: '100vw', height: '100vh'
         }}>
-          <div id='tabWhere' className='cursor-pointer shadow-xl rounded-xl p-4 bg-white' style={{ margin: '1rem 0' }} onClick={openTab}>
+          <div id='tabWhere' className='cursor-pointer shadow-[0_1px_10px_rgba(0,0,0,0.1)] rounded-xl p-4 bg-white' style={{ margin: '1rem 0' }} onClick={openTab}>
             <div className='flex justify-between'>
-              {!openWhere && <h3>Where</h3>}
-              {openWhere && <h2>Where to?</h2>}
-              <div>
-                {!openWhere && whereTo === '' && <p>I&apos;m flexible</p>}
-
-              </div>
+              {openWhere ? <h2 style={{ marginBottom: '0.5rem' }}>Where to?</h2> : <h3>Where</h3>}
+              {!openWhere &&
+                <div>
+                  {whereTo === '' ? <p>I&apos;m flexible</p> : <h3>{whereTo}</h3>}
+                </div>}
+            </div>
+            <div>
+              <SearchAddress showWhere={showWhere} openWhere={openWhere} whereTo={whereTo} setWhereTo={setWhereTo} />
             </div>
           </div>
-          <div id='tabWhen' className='cursor-pointer shadow-xl rounded-xl p-4 bg-white' style={{ margin: '1rem 0' }} onClick={openTab}>
+          <div id='tabWhen' className='cursor-pointer shadow-[0_1px_10px_rgba(0,0,0,0.1)] rounded-xl p-4 bg-white' style={{ margin: '1rem 0' }} onClick={openTab}>
             <div className='flex justify-between'>
-              {!openWhen && <h3>When</h3>}
-              {openWhen && <h2>When are you available?</h2>}
-              <div>
-                {whenDay === 0 && !openWhen && <p>Add date</p>}
-                {whenDay !== 0 && !openWhen && <h3>{whenDay} {months[whenMonth]} {whenYear}</h3>}
-              </div>
+              {openWhen ? <h2>When are you available?</h2> : <h3>When</h3>}
+              {!openWhen &&
+                <div>
+                  {whenDay === 0 ? <p>Add date</p> : <h3>{whenDay} {months[whenMonth]} {whenYear}</h3>}
+                </div>}
             </div>
             {openWhen &&
               <div style={{ position: 'relative' }}>
@@ -201,23 +192,27 @@ export default function Home() {
                 <Calendar year={year} month={month} selDay={whenDay} selMonth={whenMonth} selYear={whenYear} setDay={setWhenDay} setMonth={setWhenMonth} setYear={setWhenYear} />
               </div>}
           </div>
-          <div id='tabWho' className='cursor-pointer shadow-xl rounded-xl p-4 bg-white' style={{ margin: '1rem 0' }} onClick={openTab}>
+          <div id='tabWho' className='cursor-pointer shadow-[0_1px_10px_rgba(0,0,0,0.1)] rounded-xl p-4 bg-white' style={{ margin: '1rem 0' }} onClick={openTab}>
             <div className='flex justify-between'>
-              {!openWho && <h3>Who</h3>}
-              {openWho && <h2>Who&apos;s coming?</h2>}
-              <div>
-                {whoNum === 0 && <p>Add number</p>}
-                {whoNum > 0 && !openWho && <h3>{whoNum}</h3>}
-              </div>
+              {openWho ? <h2>Who&apos;s coming?</h2> : <h3>Who</h3>}
+              {!openWho &&
+                <div>
+                  {whoNum === 0 ? <p>Add number</p> : <h3>{whoNum}</h3>}
+                </div>}
             </div>
             {openWho &&
               <div>
                 <NumInput setValue={setWhoNum} initial={whoNum} />
               </div>}
           </div>
-          <div onClick={() => { setsearchClicked(false) }} className='cursor-pointer bg-gray-300 p-4 text-center rounded-xl hover:bg-gray-400' style={{
+          <div onClick={() => {
+            setsearchClicked(false)
+            setOpenWhere(true)
+            setOpenWhen(false)
+            setOpenWho(false)
+          }} className='duration-200 cursor-pointer bg-gray-300 p-4 text-center rounded-xl hover:text-white hover:bg-[var(--main)]' style={{
             width: '100%'
-          }}>search</div>
+          }}>Search</div>
         </div>
       }
     </>
