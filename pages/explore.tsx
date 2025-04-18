3e6250c8ed1a72602209beea58b7270570affaa9
Soonyoung Park <soon.syp@gmail.com>
2025-04-18 23:55:51 +0200

    arrange order

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -18,13 +18,13 @@ interface ExhibitionData {
 export default function Explore({ initialData = [] }: { initialData: ExhibitionData[] }) {
   return (
     <>
-      <Navigation />
-      <Searchbar/>
       <div className='exhibition mt-28 mb-16 md:mt-24 md:mb-0 py-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-x-5 gap-y-8'>
         {initialData.map((exhibition) => (
           <Exhibition key={exhibition.id} json={exhibition} />
         ))}
       </div>
+      <Navigation />
+      <Searchbar />
     </>
   )
 }
