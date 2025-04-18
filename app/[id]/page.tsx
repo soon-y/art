1611d00e6a6737e6ab8b61fc160d6519fe79a2fe
Soonyoung Park <soon.syp@gmail.com>
2025-04-18 18:33:18 +0200

    fix type mismatch

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,15 +1,10 @@
 import { createClient } from '@/utils/supabase/server'
 import DetailPage from '@/pages/detail-client'
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function Page({ params }: Props) {
+export default async function Page({ params }: Awaited<{ params: { id: string } }>) {
   const supabase = await createClient()
-  const readableTitle = params.id.replace(/_/g, ' ')
+  const { id } = params
+  const readableTitle = id.replace(/_/g, ' ')
   const { data: exhibition } = await supabase.from("exhibition").select("*").eq("title", readableTitle).single()
 
   return(
