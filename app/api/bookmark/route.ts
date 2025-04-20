daacf800295dd382673dad43b702fa7f8bfbf261
Soonyoung Park <soon.syp@gmail.com>
2025-04-20 19:26:31 +0200

    return NextResponse

--- app/api/bookmark/route.ts
+++ app/api/bookmark/route.ts
@@ -3,16 +3,15 @@ import { createClient } from '@/utils/supabase/server'
 
 export async function PUT(request: Request){
   const supabase = await createClient()
-  const { id, bookmark, time }: { id: number; bookmark: boolean; time: string } = await request.json();
+  const { id, bookmark, time }: { id: number; bookmark: boolean; time: string } = await request.json()
 
-  try {
-    const { error } = await supabase
+    const { data, error } = await supabase
       .from('exhibition')
       .update({ bookmark: !bookmark, bookmark_time: time })
       .eq('id', id)
-    if (error) throw error
-    return NextResponse.json({ message: 'Bookmark updated successfully' });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
-  }
+    
+    if (error) {
+      return NextResponse.json({ success: false, error }, { status: 500 })
+    }
+    return NextResponse.json({ success: true, data })
 }
