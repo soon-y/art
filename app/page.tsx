ddac637177472764dd3560e16972a0f300bc1c2a
Soonyoung Park <soon.syp@gmail.com>
2025-04-21 22:02:06 +0200

    delete past exhibition

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,41 @@ import { createClient } from '@/utils/supabase/server'
 import Explore from '@/pages/explore'
 
 export default async function Home() {
-  const supabase = await createClient();
-  const { data: exhibitions } = await supabase.from("exhibition").select('*')
+  const supabase = await createClient()
+  const { data: exhibitions, error } = await supabase.from("exhibition").select("*")
+
+  if (error) {
+    console.error("Error fetching exhibitions:", error)
+    return
+  }
+
+  const today = new Date()
+
+  for (const ex of exhibitions || []) {
+    const dateTo = new Date(ex.date_to)
+    if (dateTo < today) {
+
+      const { id, ...el } = ex
+      const { error: insertError } = await supabase.from("exhibition_past").insert(el)
+
+      if (insertError) {
+        console.error("Insert error:", insertError.message)
+        console.log("Offending data:", ex)
+      }
+      
+      const { error: deleteError } = await supabase.from("exhibition").delete().eq("id", ex.id)
+
+      if (deleteError) {
+        console.error(`Error deleting exhibition ${ex.id}:`, deleteError)
+      }
+    }
+  }
+
+  const { data: exhibition } = await supabase.from("exhibition").select("*")
 
   return (
     <>
-      <Explore initialData={exhibitions ?? []} />
+      <Explore initialData={exhibition ?? []} />
     </>
   )
 }
