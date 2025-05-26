d94af05c2cb169ea1c574be6eb772276cafd3189
Soonyoung Park <soon.syp@gmail.com>
2025-05-26 22:09:58 +0200

    replace the icon with image

--- components/account.tsx
+++ components/account.tsx
@@ -27,16 +27,26 @@ const Account = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <div className="h-[40px] w-[40px] p-0 cursor-pointer flex items-center justify-center">
-          <UserRound size={ICON_SIZE} className="cursor-pointer text-muted-foreground" />
+        <div className='h-[32px] w-[32px] bg-cover bg-center rounded-full cursor-pointer' style={{
+          backgroundImage: `url('https://picsum.photos/id/64/100/100')`
+        }}>
         </div>
+        {/* <div className="h-[40px] w-[40px] p-0 cursor-pointer flex items-center justify-center">
+          <UserRound size={ICON_SIZE} className="cursor-pointer text-muted-foreground" />
+        </div> */}
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-content mt-4" align="end">
+      <DropdownMenuContent className="w-content mt-1" align="end">
         <DropdownMenuRadioGroup>
-          <Link href={'/sign-in'}>
+          {/* <Link href={'/sign-in'}>
             <DropdownMenuRadioItem className="flex gap-2" value="light">
               <LogIn size={ICON_SIZE} />
               <span>Log in</span>
+            </DropdownMenuRadioItem>
+          </Link> */}
+          <Link href={'/profile'}>
+            <DropdownMenuRadioItem className="flex gap-2" value="light">
+              <UserRound size={ICON_SIZE} />
+              <span>Profile</span>
             </DropdownMenuRadioItem>
           </Link>
           <Link href={'/bookmarks'}>
