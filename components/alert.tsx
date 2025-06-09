251073ab3336f23817c9c8dbd062d51b04de4ca4
Soonyoung Park <soon.syp@gmail.com>
2025-06-09 21:43:27 +0200

    change bg color

--- components/alert.tsx
+++ components/alert.tsx
@@ -7,7 +7,7 @@ export default function Alert({msg} : {msg: ReactNode}) {
 
   return (
       <div className='absolute left-0 top-0 w-screen h-screen flex backdrop-blur-sm z-1000'>
-        <div className='bg-gray-100 rounded-xl p-6 m-auto flex flex-col items-center shadow-xl'>
+        <div className='bg-white rounded-xl p-6 m-auto flex flex-col items-center shadow-xl'>
         <TriangleAlert className='w-20 h-10 text-[#ff9100]'/>
         <p className='text-center font-medium mt-2 text-gray-800'>
           {msg}
