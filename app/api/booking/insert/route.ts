6c36e71bca95e05646c1c0f30dde40240dfee419
Soonyoung Park <soon.syp@gmail.com>
2025-06-12 15:35:15 +0200

    update

--- app/api/booking/insert/route.ts
+++ app/api/booking/insert/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: NextRequest) {
 
     const { data: lastBooking, error: newBookingIDerror } = await supabase.from('booking').select('id').order('id', { ascending: false }).limit(1)
 
-    const newBookingID = lastBooking?.[0]?.id + 1
+    const newBookingID = (lastBooking?.[0]?.id ?? 0) + 1
 
     const notification = {
       booking_id: newBookingID,
@@ -16,6 +16,7 @@ export async function POST(request: NextRequest) {
     }
 
     const { data: bookingData, error: bookingError } = await supabase.from('booking').insert([newBooking])
+
     const { data: notificationData, error: notificationError } = await supabase.from('notification').insert([notification])
 
     if (bookingError || notificationError || newBookingIDerror) {
@@ -25,11 +26,13 @@ export async function POST(request: NextRequest) {
           error: {
             booking: bookingError?.message,
             notification: notificationError?.message,
+            id: newBookingIDerror?.message,
           },
         },
         { status: 500 }
       )
     }
+
     return NextResponse.json({ success: true, bookingData, notificationData })
   } catch {
     return NextResponse.json({ success: false, error: 'Failed to insert' }, { status: 500 })
