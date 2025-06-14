dce0f170b1fa0b6ffe700fb17a81b5a6fe39665d
Soonyoung Park <soon.syp@gmail.com>
2025-06-14 10:42:05 +0200

    update styles

--- components/alert.tsx
+++ components/alert.tsx
@@ -7,11 +7,11 @@ export default function Alert({msg} : {msg: ReactNode}) {
 
   return (
       <div className='fixed left-0 top-0 w-screen h-screen flex backdrop-blur-sm z-10'>
-        <div className='bg-muted border rounded-xl p-6 m-auto flex flex-col items-center shadow-xl'>
+        <div className='bg-muted border rounded-xl px-6 py-4 m-auto flex flex-col items-center shadow-xl'>
         <TriangleAlert className='w-20 h-10 text-[#ff9100]'/>
-        <p className='text-center font-medium mt-2 text-foreground'>
+        <div className='text-center mt-2 text-foreground'>
           {msg}
-        </p>
+        </div>
         </div>
       </div>
   )
